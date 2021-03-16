4de9ad163e330f892d659e2140bcb65b1b16c59e
belalali0626 <1915996@brunel.ac.uk>
2021-03-16 02:50:36 +0000

    twoSum

--- leetcode/src/leetcode/twoSum.java
+++ leetcode/src/leetcode/twoSum.java
@@ -1,5 +1,27 @@
 package leetcode;
 
-public class twoSum {
 
+public class twoSum{
+	public static void main(String[] args) {
+	
+		int[] numbers = new int[] { 2, 3, 7, 4 ,8};;
+		int target = 7;
+		int[] result = getTwoSum(numbers, target);
+		System.out.print(result[0] +" " + result[1]);
+	}
+
+	    public static int[] getTwoSum(int[] nums, int target) {
+	    	
+	    	for (int i=0; i<nums.length;i++)	{
+	    		for(int j=i+1; j<nums.length; j++) {
+	    			int complement = target - nums[i];
+	    					
+	    			if(nums[j] == complement) {
+	    				return new int[] {i, j };
+	    			}
+	    		}
+	    	}
+	        
+	    	throw new IllegalArgumentException("no match found");
+	    }
 }
